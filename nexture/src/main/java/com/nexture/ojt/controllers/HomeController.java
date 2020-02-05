86f4f174e4793aedcd1815477a756cd93bf48481
MyeongkookPark <myeongkookpark@MyeongKook-Parkui-MacBookPro.local>
2020-02-05 22:57:30 +0900

    Master Setting

---
+++ nexture/src/main/java/com/nexture/ojt/controllers/HomeController.java
@@ -0,0 +1,11 @@
+package com.nexture.ojt.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+}
